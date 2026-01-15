0db56faed3f739de7d507c292500ff17e11684b5
PH3-Digital <philipeoliveira1984@gmail.com>
2026-01-15 01:51:19 -0300

    fix(dto): Atualiza DTOs para espelhar contrato de 19 campos da API Python + GenAI Strategy

--- src/main/java/com/nexthorizon/churnInsight_api/dto/ChurnPredictionRequest.java
+++ src/main/java/com/nexthorizon/churnInsight_api/dto/ChurnPredictionRequest.java
@@ -1,19 +1,94 @@
 package com.nexthorizon.churnInsight_api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChurnPredictionRequest {
-    @NotNull
-    private Integer tempo_contrato_meses;
 
-    @NotNull
-    private Integer atrasos_pagamento;
+    // Dados Demográficos
+    @JsonProperty("Gender")
+    @NotNull(message = "Gender is required")
+    private String gender; // "Male", "Female"
 
+    @JsonProperty("SeniorCitizen")
     @NotNull
-    private Double uso_mensal;
+    private Integer seniorCitizen; // 0, 1
 
+    @JsonProperty("Partner")
     @NotNull
-    private String plano;
+    private String partner; // "Yes", "No"
+
+    @JsonProperty("Dependents")
+    @NotNull
+    private String dependents; // "Yes", "No"
+
+    // Dados de Serviço
+    @JsonProperty("TenureMonths")
+    @NotNull
+    private Integer tenureMonths;
+
+    @JsonProperty("PhoneService")
+    @NotNull
+    private String phoneService;
+
+    @JsonProperty("MultipleLines")
+    @NotNull
+    private String multipleLines; // "Yes", "No", "No phone service"
+
+    @JsonProperty("InternetService")
+    @NotNull
+    private String internetService; // "DSL", "Fiber optic", "No"
+
+    @JsonProperty("OnlineSecurity")
+    @NotNull
+    private String onlineSecurity;
+
+    @JsonProperty("OnlineBackup")
+    @NotNull
+    private String onlineBackup;
+
+    @JsonProperty("DeviceProtection")
+    @NotNull
+    private String deviceProtection;
+
+    @JsonProperty("TechSupport")
+    @NotNull
+    private String techSupport;
+
+    @JsonProperty("StreamingTV")
+    @NotNull
+    private String streamingTV;
+
+    @JsonProperty("StreamingMovies")
+    @NotNull
+    private String streamingMovies;
+
+    // Financeiro e Contrato
+    @JsonProperty("Contract")
+    @NotNull
+    private String contract; // "Month-to-month", "One year", "Two year"
+
+    @JsonProperty("PaperlessBilling")
+    @NotNull
+    private String paperlessBilling;
+
+    @JsonProperty("PaymentMethod")
+    @NotNull
+    private String paymentMethod; // "Electronic check", ...
+
+    @JsonProperty("MonthlyCharges")
+    @NotNull
+    private Double monthlyCharges;
+
+    @JsonProperty("TotalCharges")
+    @NotNull
+    private Double totalCharges;
 }
--- src/test/java/com/nexthorizon/churnInsight_api/ChurnInsightApiApplicationTests.java
+++ src/test/java/com/nexthorizon/churnInsight_api/ChurnInsightApiApplicationTests.java
@@ -2,12 +2,12 @@ package com.nexthorizon.churnInsight_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles; // <--- Importante
 
 @SpringBootTest
+@ActiveProfiles("test") // <--- ESTA É A CHAVE MESTRA
 class ChurnInsightApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
